29d21716a90df2b372bd482a554caca2a7292657
root <root@localhost.localdomain>
2017-03-21 02:10:45 +0800

    fix bug

--- pwntools-install.sh
+++ pwntools-install.sh
@@ -3,9 +3,9 @@
 dirname=$(dirname "$0")
 
 source ${dirname}/color.sh
-echo ${green}"install pwntools..."${normal}
+echo -e ${green}"install pwntools..."${normal}
 
 apt-get update
-apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+apt-get install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
 pip install --upgrade pip
 pip install --upgrade pwntools
