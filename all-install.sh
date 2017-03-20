9acfbd3d013409b4df5248818f28ded6072422e3
OAlienO <jeffrey6910@gmail.com>
2017-03-20 23:42:24 +0800

    initial commit

---
+++ all-install.sh
@@ -0,0 +1,4 @@
+./powerline-install.sh
+./powerline-fonts-install.sh
+./docker-install.sh
+./pwntools-install.sh
---
+++ powerline-fonts-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# install fonts
+mkdir -p ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -P ~/.fonts
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -P ~/.config/fontconfig/conf.d
+fc-cache -vf ~/.fonts/
