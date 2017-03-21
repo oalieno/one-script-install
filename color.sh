486c06655b9fe6b455c598708cccf5b0a41add09
OAlienO <jeffrey6910@gmail.com>
2017-03-21 18:10:58 +0800

    some change...

--- color.sh
+++ color.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 green='\033[1;32m'
+cyan='\033[1;36m'
 normal='\033[0m'
--- zsh/oh-my-zsh-install.sh
+++ zsh/oh-my-zsh-install.sh
@@ -7,3 +7,8 @@ echo -e ${green}"install oh my zsh..."${normal}
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+echo -e ${cyan}
+echo "If you want to use the most awesome theme : agnoster"
+echo "You need to install powerline fonts ( powerline-fonts-install.sh )"
+echo -e ${normal}
