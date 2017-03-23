eba8d6105f18aec4348b843fbc8c9d2f0074289f
root <root@localhost.localdomain>
2017-03-23 16:44:54 +0800

    fix z3 install bug

--- z3-install.sh
+++ z3-install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Z3Prover/z3.git ~/z3
-python ~/z3/scripts/mk_make.py --python
-cd ~/z3/build
+cd ~/z3
+python scripts/mk_make.py --python
+cd build
 make
 make install
 cd ~
