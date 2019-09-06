bb9458ecad78cf4407f704adff8badd74ad487d8
OAlienO <jeffrey6910@gmail.com>
2019-09-06 15:58:51 +0800

    Update oh-my-zsh-install.sh

--- zsh/oh-my-zsh-install.sh
+++ zsh/oh-my-zsh-install.sh
@@ -5,8 +5,7 @@ dirname=$(dirname "$0")
 source ${dirname}/../color.sh
 echo -e ${green}"install oh my zsh..."${normal}
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo -e ${cyan}"If you want to use the most awesome theme : agnoster or powerlevel9k/powerlevel9k"
 echo -e "You need to install powerline fonts ( powerline-fonts-install.sh )"${normal}
