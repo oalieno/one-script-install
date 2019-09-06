4d0219ad39aa8667858ff71fb9008593d6c3859d
OAlienO <jeffrey6910@gmail.com>
2019-09-06 15:58:06 +0800

    Update zsh-install.sh

--- zsh/zsh-install.sh
+++ zsh/zsh-install.sh
@@ -5,8 +5,9 @@ dirname=$(dirname "$0")
 source ${dirname}/../color.sh
 echo -e ${green}"install zsh..."${normal}
 
-apt-get update
-apt-get install -y zsh
+sudo apt-get update
+sudo apt-get install -y zsh
 
 echo -e ${cyan}"If you want to use zsh as your default shell, type : "
-echo -e "chsh -s $(which zsh)"${normal}
+echo -e "chsh -s $(which zsh)"
+echo -e "if password is required, edit /etc/pam.d/chsh: auth sufficient pam_shells.so"${normal}
